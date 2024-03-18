cadb791d6dae7de5e96feb87e5c84004ea2cc7fd
19yisus <jesusitomorales70@gmail.com>
2024-03-18 00:37:42 -0400

    [ADD] circular progress indicator

--- app/src/main/java/com/example/newsapp/ui/screens/Home_screen.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/Home_screen.kt
@@ -1,17 +1,31 @@
 package com.example.newsapp.ui.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import cafe.adriel.voyager.core.registry.rememberScreen
 import cafe.adriel.voyager.core.screen.Screen
@@ -25,6 +39,9 @@ import com.example.newsapp.ui.components.common.TopBarWithButton_component
 import com.example.newsapp.ui.components.homeScreen.CarruselCategories_component
 import com.example.newsapp.ui.components.homeScreen.Carrusel_component
 import com.example.newsapp.ui.viewModel.News_viewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 class Home_screen: Screen{
 	private lateinit var navigator: Navigator
@@ -37,6 +54,7 @@ class Home_screen: Screen{
 		val vm: News_viewModel = hiltViewModel()
 		val articulos = vm.ListStateTOpArticles.collectAsState()
 		val countNews = vm.CountNews.collectAsState()
+		var isLoading by remember { mutableStateOf(true) }
 		
 		Scaffold(
 			topBar = { TopBarWithButton_component { navigator.push(Screen_Search) } }
@@ -44,26 +62,43 @@ class Home_screen: Screen{
 			Box(modifier = Modifier
 				.fillMaxSize()
 				.padding(it)){
-				LazyColumn(
-					modifier = Modifier.fillMaxSize().padding(vertical = 10.dp, horizontal = 10.dp)
-				){
-					
-					item {
-						Carrusel_component(articulos.value, navigator)
-						Spacer(modifier = Modifier.padding(bottom = 20.dp))
-					}
-					
-//					item {
-//						Dropdown_menu_component(
-//							value = countNews.value,
-//							onChange = { vm.updateCount(it) }
-//						)
-//					}
-					
-					for(dataCategorie in Categories_news.values()){
+				if(articulos.value.size > 1){
+					LazyColumn(
+						modifier = Modifier
+							.fillMaxSize()
+							.padding(vertical = 10.dp, horizontal = 10.dp)
+					){
+						
 						item {
-							CarruselCategories_component(dataCategorie, vm, navigator, countNews.value)
+							Carrusel_component(articulos.value, navigator)
+							Spacer(modifier = Modifier.padding(bottom = 20.dp))
 						}
+						
+	//					item {
+	//						Dropdown_menu_component(
+	//							value = countNews.value,
+	//							onChange = { vm.updateCount(it) }
+	//						)
+	//					}
+						
+						for(dataCategorie in Categories_news.values()){
+							item {
+								CarruselCategories_component(dataCategorie, vm, navigator, countNews.value)
+							}
+						}
+					}
+				}else{
+					Column(
+						modifier = Modifier.fillMaxSize(),
+						horizontalAlignment = Alignment.CenterHorizontally,
+						verticalArrangement = Arrangement.Center
+					){
+						LaunchedEffect(true) {
+							delay(2000)
+							isLoading = false
+						}
+						if(isLoading) CircularProgressIndicator(modifier = Modifier.size(100.dp))
+						if(!isLoading) Text(text = "No hay noticias para mostrar", fontSize = 22.sp)
 					}
 				}
 			}
